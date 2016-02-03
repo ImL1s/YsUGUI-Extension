c55b8baba89ae024bb22d2658e41f1fccb128064
ImL1s <1032448217@gm.cute.edu.tw>
2016-02-03 20:11:29 +0800

    New Script ButtonSoundUGUI And new folder: StreamingAssets
    /*
    * Ver 0.1.1
    * date:2016/02/03
    */
    - New Script ButtonSoundUGUI
    -Create a new folder: StreamingAssets,and new audioClip:buttonSound

---
+++ Scripts/YsUGUI Extension/ReadMe.txt
@@ -0,0 +1,21 @@
+﻿/*
+ * Author:ImL1s
+ * Email:ImL1s@outlook.com
+ */
+
+
+You can use this script in anywhere.
+
+
+/*
+ * Ver 0.1.1
+ * date:2016/02/03
+ */
+  - New Script ButtonSoundUGUI
+  -Create a new folder: StreamingAssets,and new audioClip:buttonSound
+
+/*
+ * Ver 0.1.0
+ * date:2016/02/03
+ */
+  -TweenAlphaUGUI
---
+++ Scripts/YsUGUI Extension/Script/ButtonSoundUGUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSoundUGUI : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private AudioClip buttonSound = null;
+
+    void Start()
+    {
+        if (buttonSound == null) StartCoroutine(LoadSound());
+    }
+
+    public void SetAudioClip(AudioClip clip)
+    {
+        this.buttonSound = clip;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        AudioSource.PlayClipAtPoint(buttonSound, Vector3.zero);
+    }
+
+
+    private IEnumerator LoadSound()
+    {
+        WWW www = new WWW("File://" + Application.streamingAssetsPath + "/Audio/buttonSound.ogg");
+        yield return www;
+
+        buttonSound = www.audioClip;
+
+        www.Dispose();
+    }
+}
