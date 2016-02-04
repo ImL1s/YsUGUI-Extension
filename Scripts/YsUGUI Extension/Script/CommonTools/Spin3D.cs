f7404b6e9c35f0ec545748c94715afa85a37e627
ImL1s <1032448217@gm.cute.edu.tw>
2016-02-05 00:23:31 +0800

    Update 3D model spin scirpt

---
+++ Scripts/YsUGUI Extension/Script/CommonTools/Spin3D.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Author:ImL1s
+ * Email:ImL1s@outlook.com
+ * Summary:
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Spin3D : MonoBehaviour
+{
+
+    private Vector3 previousV3 = default(Vector3);
+    private Vector3 orginal;
+
+    void OnMouseDrag()
+    {
+        if (Input.mousePosition.x > orginal.x + 5f)
+        {
+            orginal = Input.mousePosition;
+            transform.Rotate(Vector3.up * -5);
+        }
+        else if(Input.mousePosition.x < orginal.x -5f)
+        {
+            orginal = Input.mousePosition;
+            transform.Rotate(Vector3.up * 5);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        previousV3 = new Vector3(Screen.height / 2, Screen.width / 2); 
+    }
+
+    void OnMouseDown()
+    {
+        orginal = Input.mousePosition;
+    }
+}
